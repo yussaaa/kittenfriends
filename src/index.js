d2e3e7bc326e29927324f350a10fa15912401f1d
yussaaa <yusa.li@mail.utoronto.ca>
2023-05-27 09:15:55 -0400

    import tachyons, leave only App in render

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'tachyons';
+// import Hello from './Hello';
+// import Card from './Card';
+// import CardList from './CardList';
+// import { kittens } from './robots';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
