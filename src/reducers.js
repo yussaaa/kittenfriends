a66490f02be37d623c63f23d1e80c9712e16db23
yussaaa <yusa.li@mail.utoronto.ca>
2023-07-05 15:15:11 -0400

    Prepare actions and reducer

---
+++ src/reducers.js
@@ -0,0 +1,15 @@
+import { CHANGE_SEARCH_FIELD } from "./constants";
+
+const initialState = {
+  searchField: "",
+};
+
+export const searchKitten = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case CHANGE_SEARCH_FIELD:
+      return Object.assign({}, state, { searchField: action.payload });
+    // return { ...state, searchField: action.payload };  // <-- Object spread operator:
+    default:
+      return state;
+  }
+};
