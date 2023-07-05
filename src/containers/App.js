79e47f5c613ecd29e72b620a9de2239cbc3a5864
yussaaa <yusa.li@mail.utoronto.ca>
2023-07-05 16:39:54 -0400

    Connect on action, props and dispatch

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
-import CardList from '../components/CardList';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+import CardList from "../components/CardList";
 // import { kittens } from './robots';
-import SearchBox from '../components/searchBox'
+import SearchBox from "../components/searchBox";
 
-import './App.css'
-import Scroll from '../components/Scroll'
-import ErrorBoundary from '../components/ErrorBoundary';
+import "./App.css";
+import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 
+import { setSearchField } from "../actions";
+import { connect } from "react-redux";
 
 // const state = {
 //   kittens: kittens,
@@ -25,64 +27,71 @@ import ErrorBoundary from '../components/ErrorBoundary';
 //   )
 // }
 
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  };
+};
+
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       kittens: [],
-      searchfield: ''
-    }
+      // searchfield: "",
+    };
     // console.log('Constructor')
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-    // searchfield = event.target.value
+  // onSearchChange = (event) => {
+  //   this.setState({ searchfield: event.target.value });
+  //   // searchfield = event.target.value
 
-    console.log({ event });
-  }
+  //   console.log({ event });
+  // };
 
   componentDidMount() {
     // console.log("componentDidMount")
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => { this.setState({ kittens: users }) })
-
+    console.log(this.props.store);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ kittens: users });
+      });
   }
 
-
   render() {
+    const { kittens } = this.state;
+    const { searchField, onSearchChange } = this.props;
 
-    const { kittens, searchfield } = this.state
-
-    const filteredKittens = kittens.filter(kitten => {
-      return kitten.name.toLowerCase().includes(
-        searchfield.toLocaleLowerCase()
-      );
+    const filteredKittens = kittens.filter((kitten) => {
+      return kitten.name
+        .toLowerCase()
+        .includes(searchField.toLocaleLowerCase());
     });
 
     // console.log('Render')
 
-    return !kittens.length ?
-      <h1 className='f1'>Loading ...</h1> :
-
-      (
-        <div className='tc'>
-          <h1 className='f1'>Kitten Friends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <ErrorBoundary>
-              <CardList kittens={filteredKittens} />
-            </ErrorBoundary>
-
-          </Scroll>
-
-
-        </div>
-      )
+    return !kittens.length ? (
+      <h1 className="f1">Loading ...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">Kitten Friends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList kittens={filteredKittens} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
   }
-
 }
 
-
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
