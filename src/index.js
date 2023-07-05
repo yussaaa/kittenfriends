f18cbd6abed2cf47fb94791c3712a34b605c2b6b
yussaaa <yusa.li@mail.utoronto.ca>
2023-07-05 15:24:02 -0400

    Wrap App with a Provider component and make store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./containers/App";
+import reportWebVitals from "./reportWebVitals";
 
-import 'tachyons';
+import "tachyons";
 // import Hello from './Hello';
 // import Card from './Card';
 // import CardList from './CardList';
 // import { kittens } from './robots';
+import { Provider } from "react-redux"; //Provider here as a wrapper to pass the store to every object underneath
+import { createStore } from "redux";
+import { searchKitten } from "./reducers";
 
+const store = createStore(searchKitten); // In real cases, there will be multiple reducers
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
